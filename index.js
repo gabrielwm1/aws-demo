8977952faf75a39c3bcd426cdcc2194852e8b2b4
Gabe Wilk <gabe.wilk@gmail.com>
2020-10-03 23:20:25 -0400

    save user to mongo db with googleid

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-require("./services/passport");
-const mongoose = requir("mongoose");
-mongoose.connect(keys.mongoURI);
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
 const app = express();
 
+require("./models/user");
+require("./services/passport");
 require("./routes/authRoutes")(app);
+mongoose.connect(keys.mongoURI);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+//represents a record inside the db
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +13,8 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
+    (accessToken, refreshToken, profile) => {
+      new User({ googleId: profile.id }).save();
     }
   )
 );
