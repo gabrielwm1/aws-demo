a8243c00bafc6f131ee8359966861d583c58b4b7
Gabe Wilk <gabe.wilk@gmail.com>
2020-10-04 19:56:54 -0400

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   //  "/api/*"
   app.use(
-    ["/api", "/auth/google", "api/*"],
+    ["api/*", "/auth/google"],
     createProxyMiddleware({
       target: "http://localhost:5000",
     })
